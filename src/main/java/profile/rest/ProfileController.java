1f1446d5da02420a33294eb31f95e15a917d7918
GiuseppeProment <giuseppe.proment@gmail.com>
2016-08-27 18:58:25 -0300

    adjustes

--- src/main/java/profile/rest/ProfileController.java
+++ src/main/java/profile/rest/ProfileController.java
@@ -2,6 +2,7 @@ package profile.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -23,6 +24,17 @@ public class ProfileController {
     
 	@Autowired
 	ProfileService service;
+
+	@RequestMapping(value="/",produces=MediaType.TEXT_HTML_VALUE)
+	public String hello() {
+		return "<!DOCTYPE html>"
+				+ "<html><body>"
+				+ "<b>Available urls</b>"
+				+ "<p>/cadastro (post with json body)</p>"
+				+ "<p>/login/email/password</p>"
+				+ "<p>/perfil/userId</p>"
+				+ "</body></html>";		
+	}
 	
     @RequestMapping(value="/cadastro", method=RequestMethod.POST )
     @ResponseStatus(HttpStatus.CREATED)
